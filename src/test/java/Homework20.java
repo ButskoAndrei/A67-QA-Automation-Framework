27b30caad18463abdd9f3321d336e86745c4a62a
ButskoAndrei <andrei.butsko@testpro.io>
2025-02-11 11:42:39 -0500

    Homework20

---
+++ src/test/java/Homework20.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Homework20 extends BaseTest{
+
+    @Test
+    public void deletePlaylist() {
+
+        String expectedPlaylistDeleteMessage = "Deleted playlist \"Test 1.\"";
+
+        String username = "andrei.butsko@testpro.io";
+        String password = "SignZ1ex";
+
+        login(username,password);
+        clickChoosePlaylist();
+        clickDeleteBtn();
+        //clickOkButton();
+        Assert.assertEquals(getDeletePlaylistMessage(),expectedPlaylistDeleteMessage);
+
+    }
+
+    public void clickChoosePlaylist() {
+        WebElement choosePlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//nav[@id='sidebar']//a[contains(text(),'Test 1')]")));
+        choosePlaylist.click();
+    }
+
+    public void clickDeleteBtn() {
+        WebElement deleteBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-test='song-list-controls']//button[@title='Delete this playlist']")));
+        deleteBtn.click();
+    }
+
+    /*public void clickOkButton() throws InterruptedException {
+        WebElement okButton = driver.findElement(By.xpath("//div[@class='dialog']//button[@class='ok']"));
+        okButton.click();
+        Thread.sleep(2000);
+    }*/
+
+    public String getDeletePlaylistMessage(){
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        return notification.getText();
+    }
+
+}
